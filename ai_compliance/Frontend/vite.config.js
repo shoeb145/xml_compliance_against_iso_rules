542a961dcf493c530f04104097f35470c1278b64
Shrenik027 <shrenik9477@gmail.com>
2025-09-27 19:52:58 +0530

    added server

--- ai_compliance/Frontend/vite.config.js
+++ ai_compliance/Frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend", // or '/xml-compliance-against-iso-rule2/' if you want to keep that path
+  base: "/", // or '/xml-compliance-against-iso-rule2/' if you want to keep that path
 });
